12b84747ae161f9dbdb66e67d24550441e8d0d37
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-08-29 08:50:19 -0300

    Add files via upload

---
+++ banco-xaml/banco-xaml/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace banco_xaml
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Login efetuado");
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Ajuste adicional do layout pode ser feito aqui, se necessário
+        }
+
+    }
+}
